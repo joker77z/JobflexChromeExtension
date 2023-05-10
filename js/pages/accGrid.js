18b8478efca09419b88dc27e7ceb31d16c93a09a
ptj0125 <ptj0125@midasin.com>
2023-05-10 20:48:21 +0900

    refactor: UI 정리

--- js/pages/accGrid.js
+++ js/pages/accGrid.js
@@ -1,59 +1,96 @@
 (async () => {
-  const src = chrome.runtime.getURL("js/helper/common.js");
+  const origin = window.location.origin;
+  const accStatus = ['완료', '미응시', '미완료', '접속횟수 초과'];
+
+  const src = chrome.runtime.getURL('js/helper/common.js');
   const commonScript = await import(src);
-
-  console.log(commonScript.matchUrlToRun('/mrs2/manager/screening/list'));
-
-  if(!commonScript.matchUrlToRun('/mrs2/manager/screening/list')) {
+  if (!commonScript.matchUrlToRun('/mrs2/manager/screening/list')) {
     return false;
   }
 
   chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
-    if(!document.querySelector('.gridH1')) return;
+    if (!document.querySelector('.gridH1')) return;
 
     if (msg.from === 'popup') {
       switch (msg.action) {
         case 'selectedPersonAddData':
           selectedPersonAddData(sendResponse);
+          break;
         case 'allPersonAddData':
           allPersonAddData(sendResponse);
+          break;
       }
     }
   });
 
+  function allPersonAddData(sendResponse) {
+    // 전형번호는 빈 배열, 응시자 번호를 보낸다.
+    const checkedData = {
+      checkedScreeningResumeSn: [],
+      checkedScreeningSn: 0,
+    };
+
+    [...document.querySelectorAll('input.checkbox[name="resumeSn"]')].forEach((checkbox) => {
+      const checkedScreeningResumeSn = checkbox.getAttribute('data-screeningresumesn');
+      const accStatusButton = document.querySelector(`button[data-screeningresumesn="${checkedScreeningResumeSn}"]`);
+      const accStatusText = accStatusButton.previousSibling.textContent;
+
+      if (!accStatus.includes(accStatusText)) {
+        alert('응시 데이터를 추가하려는 지원자 중에 "완료", "미용시", "미완료", "접속횟수 초과" 상태가 아닌 지원자가 있습니다!');
+        return;
+      }
+
+      checkedData.checkedScreeningResumeSn.push(checkedScreeningResumeSn);
+
+      if (!checkedData.checkedScreeningSn) {
+        checkedData.checkedScreeningSn = Number(checkbox.getAttribute('data-screeningsn'));
+      }
+    });
+
+    // fetch(`${origin}/chrome-extension/extend-password-expiration-date`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(checkedData),
+    // });
+
+    console.log(checkedData);
+  }
+
   function selectedPersonAddData(sendResponse) {
     // 체크된 전형번호, 응시자 번호를 읽는다.
     // 이 때 만약 완료, 미응시, 미완료, 접속횟수 초과가 포함되어 있다면 Alert를 띄운다.
-    let checkedScreeningResumeSn
-    [...document.querySelectorAll('input.checkbox:checked')].forEach(checkbox => {
-      checkbox.getAttribute('data-screeningsn')
-    })
-  }
+    const checkedData = {
+      checkedScreeningResumeSn: [],
+      checkedScreeningSn: 0,
+    };
 
-  function allPersonAddData(sendResponse) {
-    // 전형번호는 빈 배열, 응시자 번호를 보낸다.
+    [...document.querySelectorAll('input.checkbox:checked')].forEach((checkbox) => {
+      const checkedScreeningResumeSn = checkbox.getAttribute('data-screeningresumesn');
+      const accStatusButton = document.querySelector(`button[data-screeningresumesn="${checkedScreeningResumeSn}"]`);
+      const accStatusText = accStatusButton.previousSibling.textContent;
 
+      if (!accStatus.includes(accStatusText)) {
+        alert('응시 데이터를 추가하려는 지원자 중에 "완료", "미용시", "미완료", "접속횟수 초과" 상태가 아닌 지원자가 있습니다!');
+        return;
+      }
+
+      checkedData.checkedScreeningResumeSn.push(checkedScreeningResumeSn);
+
+      if (!checkedData.checkedScreeningSn) {
+        checkedData.checkedScreeningSn = Number(checkbox.getAttribute('data-screeningsn'));
+      }
+    });
+
+    // fetch(`${origin}/chrome-extension/extend-password-expiration-date`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(checkedData),
+    // });
+
+    console.log(checkedData);
   }
-  
-  // chrome.storage.local.get(['alwaysDontViewPopup']).then((result) => {
-  //   if (result.alwaysDontViewPopup) {
-  //     dontViewPopup();
-  //   }
-  // });
-  
-  // function dontViewPopup(sendResponse) {
-  //   let timer = setInterval(() => {
-  //     if (document.querySelector('#NoticepopUp')) {
-  //       removeDOM();
-  //       if (sendResponse) sendResponse({ success: true });
-  //       clearInterval(timer);
-  //     }
-  //   }, 500);
-  
-  //   function removeDOM() {
-  //     [...document.querySelectorAll('#NoticepopUp')].forEach((element) => {
-  //       element.remove();
-  //     });
-  //   }
-  // }
 })();
--- js/pages/selectProduct.js
+++ js/pages/selectProduct.js
@@ -1,43 +1,41 @@
 (async () => {
-  const src = chrome.runtime.getURL("js/helper/common.js");
+  const src = chrome.runtime.getURL('js/helper/common.js');
   const commonScript = await import(src);
 
   console.log(commonScript.matchUrlToRun('/cus/selectProduct'));
 
-  if(!commonScript.matchUrlToRun('/cus/selectProduct')) {
+  if (!commonScript.matchUrlToRun('/cus/selectProduct')) {
     return false;
   }
 
   chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
     console.log('message received');
-  
+
     if (msg.from === 'popup') {
       switch (msg.action) {
         case 'alwaysDontViewLoginHistory':
           dontViewLoginHistory(sendResponse);
+          break;
         case 'alwaysDontViewPopup':
           dontViewPopup(sendResponse);
+          break;
         case 'alwaysExtendPassword':
         case 'extendPassword':
           extendPassword(sendResponse);
           break;
-        case 'selectedPersonAddData':
-          selectedPersonAddData(sendResponse);
-        case 'allPersonAddData':
-          allPersonAddData(sendResponse);
       }
     }
   });
-  
+
   const storageKeysToFunctionMap = {
     alwaysDontViewLoginHistory: dontViewLoginHistory,
     alwaysDontViewPopup: dontViewPopup,
     alwaysExtendPassword: extendPassword,
   };
-  
+
   Object.entries(storageKeysToFunctionMap).forEach(([key, fn]) => {
     console.log('mapping');
-  
+
     chrome.storage.local.get([key]).then((result) => {
       if (result[key]) {
         fn();
@@ -46,10 +44,10 @@
       }
     });
   });
-  
+
   function dontViewLoginHistory(sendResponse) {
     console.log('함수 실행');
-  
+
     if (sendResponse) {
       const modalTitle =
         document.querySelector('.h1.face-lift-h1') ||
@@ -61,10 +59,10 @@
     }
     document.cookie = 'WELCOME_PAGE=done';
   }
-  
+
   function dontViewPopup(sendResponse) {
     console.log('함수 실행');
-  
+
     let timer = setInterval(() => {
       if (document.querySelector('[data-popup]')) {
         removeDOM();
@@ -72,19 +70,19 @@
         clearInterval(timer);
       }
     }, 500);
-  
+
     function removeDOM() {
       [...document.querySelectorAll('[data-popup]')].forEach((element) => {
         element.remove();
       });
     }
   }
-  
+
   async function extendPassword(sendResponse) {
     console.log('함수 실행');
-  
+
     const origin = window.location.origin;
-  
+
     // 비밀번호 만료가 되었는지 먼저 체크. 만료 되었으면 연장api를 실행.
     const expiredPasswordResponse = await fetch(
       `${origin}/cus/member/isExpiredPassword`,
@@ -94,33 +92,32 @@
     );
     const isExpiredPassword = await expiredPasswordResponse.json();
     if (!isExpiredPassword) return;
-  
+
     await fetch(`${origin}/chrome-extension/extend-password-expiration-date`, {
       method: 'POST',
     });
-  
+
     // 페이지 진입 전 Always ON이면 local storage데이터를 읽어서 사전에 API를 쏘기 때문에 모달이 안뜬다. 즉, sendResponse가 필요없다.
     if (sendResponse) {
       sendResponse({ success: true });
       document.querySelector('[data-type="changePassword"]').remove();
     }
   }
-  
+
   function selectedPersonAddData(sendResponse) {
     // 1. 현재 역검센터 전형 결과 그리드인지 확인한다.
     // 2. 역검센터 전형 결과 그리드면 screeningSn, 체크한 screeningResumeSn들을 읽어온다.
     //    이 때 완료, 미응시, 미완료, 접속횟수 초과인지 확인한다.
-    // 3. 
-  
+    // 3.
     /**
      * if (!현재 역검센터 전형 결과 그리드) return;
-     * 
+     *
      * const origin = window.location.origin;
      * const screeningSn = document.querySelector('#screeningSn');
      * const checkAccResume = ['완료', '미응시', '미완료', '접속횟수 초과'];
      * let selectedScreeningResumeSn = [];
      * let data = {};
-     * 
+     *
      * [...document.querySelectorAll('input #screeningResumeSn')].forEach((el) => {
      *   if(!checkAccResume.includes(el.friend.textContent)) {
      *     alert('완료, 미응시, 미완료, 접속횟수 초과에 해당하는 응시자 데이터만 데이터 추가 가능합니다.');
@@ -128,12 +125,12 @@
      *   }
      *   if(el.checked) selectedScreeningResumeSn.push(el.value);
      * });
-     * 
+     *
      * fetch(`${origin}/chrome-extension/extend-password-expiration-date`, {method: 'POST', data: data});
      */
   }
-  
+
   // function allPersonAddData(sendResponse) {
-  
+
   // }
 })();
