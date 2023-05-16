c7c144a85190725015d3e607c26f8677c93b78a1
TaejoonPark <joker77z@naver.com>
2023-05-16 18:01:21 +0900

    feat: Jobflex extension 전용 modal 제작 중

--- js/helper/common.js
+++ js/helper/common.js
@@ -4,74 +4,74 @@
  */
 function matchUrlToRun(path) {
   const domainList = [
-    'http://demo01-cms-recruiter-co-kr.kr-dv-midasitwebsol.com:3000',
-    'http://demo01-cms-recruiter-co-kr.kr-dv-midasitwebsol.com:8080',
-    'https://demo01-cms-recruiter-co-kr.kr-dv-midasitwebsol.com',
-    'https://st-jobflexqaqc-cms-recruiter-co-kr.midasweb.net',
-    'https://st-smw1020-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jobflex-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jobflexqa04-cms-recruiter-co-kr.midasweb.net',
-    'https://st-sec01-cms-recruiter-co-kr.midasweb.net',
-    'https://st-sec001-cms-recruiter-co-kr.midasweb.net',
-    'https://st-sec02-cms-recruiter-co-kr.midasweb.net',
-    'https://st-kjm07-cms-recruiter-co-kr.midasweb.net',
-    'https://st-sec04-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jobda02-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jobda03-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jobda04-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jobda05-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jobda06-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jobda07-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jobda08-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jobda09-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jobda10-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jf2-cms-recruiter-co-kr.midasweb.net',
-    'https://st-smw10203-cms-recruiter-co-kr.midasweb.net',
-    'https://st-simson-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jax20-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jax20isd-cms-recruiter-co-kr.midasweb.net',
-    'https://st-11325re-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jax10-cms-recruiter-co-kr.midasweb.net',
-    'https://st-jax20itv-cms-recruiter-co-kr.midasweb.net',
-    'https://st-sec03-cms-recruiter-co-kr.midasweb.net',
-    'https://st-inair01-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-jf-smw1020-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-jobflex01-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-jobflex02-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-jobisd-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-jobitv-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-interview01-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-jobflex-cms-recruiter-co-kr.midasweb.net',
-    'https://test02-cms-recruiter-co-kr.midasweb.net',
-    'https://acc01-all-cms-recruiter-co-kr.midasweb.net',
-    'https://acc02-integ-cms-recruiter-co-kr.midasweb.net',
-    'https://acc03-sr-game-cms-recruiter-co-kr.midasweb.net',
-    'https://acc04-sr-cms-recruiter-co-kr.midasweb.net',
-    'https://acc05-v2-cms-recruiter-co-kr.midasweb.net',
-    'https://acc06-custom-cms-recruiter-co-kr.midasweb.net',
-    'https://pdm-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-jobflex2-02-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-deletetest-cms-recruiter-co-kr.midasweb.net',
-    'https://qa-jobflex03-cms-recruiter-co-kr.midasweb.net',
-    'https://qa-jobflex04-cms-recruiter-co-kr.midasweb.net',
-    'https://qa-jobda02-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-inair01-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-inair02-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-inair-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-inairisd-cms-recruiter-co-kr.midasweb.net',
-    'https://st2-inairitv-cms-recruiter-co-kr.midasweb.net',
-    'https://qa-inair04-cms-recruiter-co-kr.midasweb.net',
-    'https://qa-inair03-cms-recruiter-co-kr.midasweb.net',
-    'https://testsvc-mit213-cms-recruiter-co-kr.midasweb.net',
-    'https://st-lsh0503-cms-recruiter-co-kr.midasweb.net',
+    "http://demo01-cms-recruiter-co-kr.kr-dv-midasitwebsol.com:3000",
+    "http://demo01-cms-recruiter-co-kr.kr-dv-midasitwebsol.com:8080",
+    "https://demo01-cms-recruiter-co-kr.kr-dv-midasitwebsol.com",
+    "https://st-jobflexqaqc-cms-recruiter-co-kr.midasweb.net",
+    "https://st-smw1020-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jobflex-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jobflexqa04-cms-recruiter-co-kr.midasweb.net",
+    "https://st-sec01-cms-recruiter-co-kr.midasweb.net",
+    "https://st-sec001-cms-recruiter-co-kr.midasweb.net",
+    "https://st-sec02-cms-recruiter-co-kr.midasweb.net",
+    "https://st-kjm07-cms-recruiter-co-kr.midasweb.net",
+    "https://st-sec04-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jobda02-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jobda03-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jobda04-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jobda05-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jobda06-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jobda07-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jobda08-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jobda09-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jobda10-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jf2-cms-recruiter-co-kr.midasweb.net",
+    "https://st-smw10203-cms-recruiter-co-kr.midasweb.net",
+    "https://st-simson-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jax20-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jax20isd-cms-recruiter-co-kr.midasweb.net",
+    "https://st-11325re-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jax10-cms-recruiter-co-kr.midasweb.net",
+    "https://st-jax20itv-cms-recruiter-co-kr.midasweb.net",
+    "https://st-sec03-cms-recruiter-co-kr.midasweb.net",
+    "https://st-inair01-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-jf-smw1020-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-jobflex01-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-jobflex02-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-jobisd-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-jobitv-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-interview01-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-jobflex-cms-recruiter-co-kr.midasweb.net",
+    "https://test02-cms-recruiter-co-kr.midasweb.net",
+    "https://acc01-all-cms-recruiter-co-kr.midasweb.net",
+    "https://acc02-integ-cms-recruiter-co-kr.midasweb.net",
+    "https://acc03-sr-game-cms-recruiter-co-kr.midasweb.net",
+    "https://acc04-sr-cms-recruiter-co-kr.midasweb.net",
+    "https://acc05-v2-cms-recruiter-co-kr.midasweb.net",
+    "https://acc06-custom-cms-recruiter-co-kr.midasweb.net",
+    "https://pdm-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-jobflex2-02-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-deletetest-cms-recruiter-co-kr.midasweb.net",
+    "https://qa-jobflex03-cms-recruiter-co-kr.midasweb.net",
+    "https://qa-jobflex04-cms-recruiter-co-kr.midasweb.net",
+    "https://qa-jobda02-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-inair01-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-inair02-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-inair-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-inairisd-cms-recruiter-co-kr.midasweb.net",
+    "https://st2-inairitv-cms-recruiter-co-kr.midasweb.net",
+    "https://qa-inair04-cms-recruiter-co-kr.midasweb.net",
+    "https://qa-inair03-cms-recruiter-co-kr.midasweb.net",
+    "https://testsvc-mit213-cms-recruiter-co-kr.midasweb.net",
+    "https://st-lsh0503-cms-recruiter-co-kr.midasweb.net",
   ];
 
   const includePathDomain = domainList.map((domain) => domain + path);
   let currentUrl = window.location.href;
 
   // 맨 뒤에 #이 붙는 경우가 있음.
-  if (currentUrl.slice(-1) === '#') {
-    currentUrl = currentUrl.slice(0, currentUrl.indexOf('#'));
+  if (currentUrl.slice(-1) === "#") {
+    currentUrl = currentUrl.slice(0, currentUrl.indexOf("#"));
   }
 
   if (includePathDomain.includes(currentUrl)) {
@@ -81,4 +81,47 @@ function matchUrlToRun(path) {
   }
 }
 
-export { matchUrlToRun };
+function modal(title, description, useConfirmButton, useAlwaysConfirmButton) {
+  if (localStorage.getItem("dontViewAddAccDataModal") !== "true") {
+    // DIM
+    const dim = document.createElement("div");
+    dim.classList = "modal-bg";
+    dim.setAttribute("style", "display: block;");
+    document.body.insertAdjacentElement("beforeend", dim);
+
+    // MODAL
+    const modalWrapper = document.createElement("div");
+    modalWrapper.classList = "modal-wrapper";
+    modalWrapper.innerHTML = `
+      ${title ? `<span class="title">${title}</span>` : ""}
+      ${description ? `<p class="description">${description}</p>` : ""}
+      ${
+        useConfirmButton
+          ? `<button id="confirmButton" class="confirm-button">
+            확인
+          </button>`
+          : ""
+      }
+      ${
+        useAlwaysConfirmButton
+          ? `<button id="alwaysConfirmButton" class="always-confirm-button">
+            다시는 보지 않기
+          </button>`
+          : ""
+      }
+    `;
+
+    dim.insertAdjacentElement("beforeend", modalWrapper);
+
+    document.querySelector("#confirmButton").addEventListener("click", () => {
+      dim.remove();
+    });
+
+    document.querySelector("#alwaysConfirmButton").addEventListener("click", () => {
+      dim.remove();
+      localStorage.setItem("dontViewAddAccDataModal", true);
+    });
+  }
+}
+
+export { matchUrlToRun, modal };
--- js/pages/accGrid.js
+++ js/pages/accGrid.js
@@ -1,17 +1,16 @@
 (async () => {
   const origin = window.location.origin;
-  const accStatus = ['완료', '미응시', '미완료', '접속횟수 초과'];
 
-  const src = chrome.runtime.getURL('js/helper/common.js');
+  const src = chrome.runtime.getURL("js/helper/common.js");
   const commonScript = await import(src);
 
-  commonScript.matchUrlToRun('/mrs2/manager/screening/list')
+  commonScript.matchUrlToRun("/mrs2/manager/screening/list")
     ? console.log(
-        '%c [Jobflex Extension] 전형진행현황 페이지 정상 진입, Extension은 그리드에서 동작합니다.',
-        'color: white; background: #00C17C; padding: 10px;',
+        "%c [Jobflex Extension] 전형진행현황 페이지 정상 진입, Extension은 그리드에서 동작합니다.",
+        "color: white; background: #00C17C; padding: 10px;"
       )
-    : '';
-  if (!commonScript.matchUrlToRun('/mrs2/manager/screening/list')) {
+    : "";
+  if (!commonScript.matchUrlToRun("/mrs2/manager/screening/list")) {
     return false;
   }
 
@@ -29,18 +28,18 @@
   // }
 
   chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
-    if (msg.from === 'popup') {
+    if (msg.from === "popup") {
       switch (msg.action) {
-        case 'selectedPersonAddData':
-          if (!document.querySelector('.gridH1')) {
-            alert('평가 결과 그리드 내에서만 사용할 수 있습니다 :)');
+        case "selectedPersonAddData":
+          if (document.querySelector(".gridH1").textContent !== "역검센터전형 결과") {
+            alert("역검센터전형 평가 결과 그리드 내에서만 사용할 수 있습니다 :)");
             return false;
           }
           selectedPersonAddData(sendResponse);
           break;
-        case 'allPersonAddData':
-          if (!document.querySelector('.gridH1')) {
-            alert('평가 결과 그리드 내에서만 사용할 수 있습니다 :)');
+        case "allPersonAddData":
+          if (document.querySelector(".gridH1").textContent !== "역검센터전형 결과") {
+            alert("역검센터전형 평가 결과 그리드 내에서만 사용할 수 있습니다 :)");
             return false;
           }
           allPersonAddData(sendResponse);
@@ -56,20 +55,27 @@
       screeningSn: 0,
     };
 
-    checkedData.screeningSn = Number(document.querySelector('input.checkbox[name="resumeSn"]').getAttribute('data-screeningsn'));
-
-    alert('"완료", "미용시", "미완료", "접속횟수 초과" 상태인 지원자만 적용됩니다.');
+    checkedData.screeningSn = Number(document.querySelector('input.checkbox[name="resumeSn"]').getAttribute("data-screeningsn"));
 
     fetch(`${origin}/chrome-extension/create/acc-examination-data`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(checkedData),
     });
 
     // accGridReload();
-    console.log(`%c [Jobflex Extension] 전체 응시자 데이터 추가 성공! ${JSON.stringify(checkedData)}`, 'color: green');
+
+    commonScript.modal(
+      "응시자 데이터 추가 작업이 시작되었습니다",
+      '<strong>응시현황 상태가 "완료", "미용시", "미완료", "접속횟수 초과" 상태인 지원자만 적용됩니다.</strong> 응시자 데이터 추가 시 미응시->완료->분석가능 인원으로 전환되고 그리드에 정상적으로 나타나기까지 오랜 시간이 소요됩니다. 현재 페이지를 벗어나거나 브라우저를 종료해도 작업은 멈추지 않습니다.',
+      true,
+      true
+    );
+
+    if (sendResponse) sendResponse({ success: true });
+    console.log(`%c [Jobflex Extension] 전체 응시자 데이터 추가 성공! ${JSON.stringify(checkedData)}`, "color: green");
   }
 
   function selectedPersonAddData(sendResponse) {
@@ -82,35 +88,40 @@
 
     const checkedInput = [...document.querySelectorAll('input.checkbox[name="resumeSn"]:checked')];
     if (checkedInput.length === 0) {
-      alert('체크한 지원자가 없습니다 :(');
+      commonScript.modal("체크한 지원자가 없습니다");
       return false;
     }
 
-    checkedData.screeningSn = Number(document.querySelector('input.checkbox[name="resumeSn"]').getAttribute('data-screeningsn'));
+    checkedData.screeningSn = Number(document.querySelector('input.checkbox[name="resumeSn"]').getAttribute("data-screeningsn"));
 
     checkedInput.forEach((checkbox) => {
-      const checkedScreeningResumeSn = checkbox.getAttribute('data-screeningresumesn');
+      const checkedScreeningResumeSn = checkbox.getAttribute("data-screeningresumesn");
       const accStatusButton = document.querySelector(`button[data-screeningresumesn="${checkedScreeningResumeSn}"]`);
       const accStatusText = accStatusButton.previousSibling.textContent;
 
+      const accStatus = ["완료", "미응시", "미완료", "접속횟수 초과"];
+
       if (!accStatus.includes(accStatusText)) {
-        alert(
-          '응시 데이터를 추가하려는 지원자 중에 "완료", "미용시", "미완료", "접속횟수 초과" 상태가 아닌 지원자가 있습니다. 해당 지원자를 제외한 나머지 지원자에 대해 적용됩니다.',
+        commonScript.modal(
+          "",
+          '응시 데이터를 추가하려는 지원자 중에 "완료", "미용시", "미완료", "접속횟수 초과" 상태가 아닌 지원자가 있습니다. 해당 지원자를 제외한 나머지 지원자에 대해 적용됩니다.'
         );
+        alert();
       }
 
       checkedData.screeningResumeSns.push(Number(checkedScreeningResumeSn));
     });
 
     fetch(`${origin}/chrome-extension/create/acc-examination-data`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(checkedData),
     });
 
     // accGridReload();
-    console.log(`%c [Jobflex Extension] 선택 응시자 데이터 추가 성공! ${JSON.stringify(checkedData)}`, 'color: green;');
+    if (sendResponse) sendResponse({ success: true });
+    console.log(`%c [Jobflex Extension] 선택 응시자 데이터 추가 성공! ${JSON.stringify(checkedData)}`, "color: green;");
   }
 })();
